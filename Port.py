ab0651880c11c4dd72dfb5171bf561fff113b946
Jeff Connelly <shellreef+git@gmail.com>
2008-02-11 20:32:41 -0800

    Initial versions with only basic fields, no type information.

---
+++ Port.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# Created:20080211
+# By Jeff Connelly
+
+IN = "in"
+OUT = "out"
+INOUT = "inout"
+
+
+class Port(object):
+    def __init__(self, name, direction, type):
+        self.name = name
+        assert direction in [IN, OUT, INOUT], \
+                "Port direction %s is invalid" % (direction,)
+
+        self.direction = direction
+        self.type = type
+
+    def __str__(self):
+        return "<Port: %s, %s, %s>" % (self.name, self.direction, self.type)
+
+if __name__ == "__main__":
+    a = Port("a", IN, None)
+    b = Port("b", IN, None)
+    c = Port("c", OUT, None)
+
+    print a
+    print b
+    print c
