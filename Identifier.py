b3afefe7653a3d5a1f979dd08e05716d67ce5fae
Antonio Chavez <ajchavez03@gmail.com>
2008-02-18 00:29:08 -0800

    Updated Identifier.py: added setValue and getValue methods as well as updated the constructor for the class. The value is of type trit.

--- Identifier.py
+++ Identifier.py
@@ -1,20 +1,41 @@
 #
 # vim: set fileencoding=utf8
-#  token.py
-#  
+#  Identifier.py
+#  Creates identifier object.
 #
 #  Created by Antonio on 2/16/08.
 #
 
+from Trits import Trits
+
 class Identifier(object):
-   def __init__(self, name):
+   def __init__(self, name, value = ""):
+   '''Initialize Identifier object.
+      length field will be used to index vector from n to 0
+   '''
       self.name = name
-   
+      self.value = Trits(value)
+      self.length = len(self.value)
+      if self.length > 1: self.type = "vector"
+      else: self.type = "trit"
+      
    def __str__(self):
-      return "<Identifier:%s>" % (self.name,)
+      return "<Identifier:%s>" % (self.name,) '''print value here'''
+      
+   def setValue(self, value):
+      self.value = Trits(value)
+      self.length = len(self.value)
+      if self.length < 1 : self.type = "vector"
+      else: self.type = "trit"
+      
+   def getValue(self):
+      return self.value
+      
       
 if __name__ == "__main__":
-   a = Identifier("a")
-   b = Identifier("b")
+   a = Identifier("one", "i")
+   b = Identifier("two", "0")
+   c = Identifier("three", "01i01")
+   d = Identifier("four")
    
-   print a, b
+   print a, b, c, d
