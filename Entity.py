ee26d04d09d15c3bb397271bc730fe0b58922b16
Antonio Chavez <ajchavez03@gmail.com>
2008-02-25 16:35:17 -0800

    Added another constructor for Entity.py.  It takes the name, list of names of inports, list of names of outports, and list of names of inoutports.  The port names lists are optional parameters.

--- Entity.py
+++ Entity.py
@@ -25,6 +25,26 @@ class Entity(object):
            else:
               self.INOUT.append(p)
 
+    def __init__(self, name, inports = [], outports = [], inoutports = []):
+        """Overloaded constructor, takes the port names from the
+        input lists.
+        name: name of the entity
+        inports: list of names for the in ports
+        outports: list of names for the out ports
+        inoutports: list of names for the inout ports
+        """
+        self.name = name
+        self.IN = []
+        self.OUT = []
+        self.INOUT = []
+
+        for p in inports:
+            self.IN.append(Port.Port(p, Port.IN, None))
+        for p in outports:
+            self.OUT.append(Port.Port(p, Port.OUT, None))
+        for p in inoutports:
+            self.INOUT.append(Port.Port(p, Port.INOUT, None))
+
     def __str__(self):
         s = "<Entity: %s, %d ports" % (self.name, len(self.IN))
         i = 0
@@ -44,6 +64,7 @@ if __name__ == "__main__":
     a = Port.Port("a", Port.IN, None)
     b = Port.Port("b", Port.IN, None)
     c = Port.Port("c", Port.OUT, None)
+    d = Entity("d", ["1", "2"], ["3"], ["4"])
 
     max = Entity("max", [a,b,c])
-    print max
+    print max, d
