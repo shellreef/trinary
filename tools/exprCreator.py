c6e83ff7efd7f9bf1d0fb3e36daed2b9bdf67625
Antonio Chavez <ajchavez03@gmail.com>
2008-04-28 23:50:50 -0700

    Added comments to exprCreator, fixed some bugs and has been tested with simple input.

--- tools/exprCreator.py
+++ tools/exprCreator.py
@@ -2,7 +2,7 @@
 # vim: set fileencoding=utf8
 # Created: April 22, 2008
 #
-# Expression manipulator
+# Expression creator
 
 import sys, os
 import Trits
@@ -28,78 +28,113 @@ ci_t  = Trits.Trits("iii")
 c0_t  = Trits.Trits("000")
 c1_t  = Trits.Trits("111")
 
+valid_chars = ("i", "0", "1")
 map_t = {False:0, None:1, True:2}
 map_str = {False:"i", None:"0", True:"1"}
 
 basic_funcs = {"sd":sd, "su":su, "s01":s01, "si0":si0, "ru":ru, "rd":rd, "inv":inv}
 basic_f_t = {"sd":sd_t, "su":su_t, "s01":s01_t, "si0":si0_t, "ru":ru_t, "rd":rd_t, "inv":inv_t}
+easy_f_t = {"buf":buf_t, "ci":ci_t, "c0":c0_t, "c1":c1_t}
 
 def eval_one(crnt, func):
     ''' eval_one: the input given in crnt with function 'func'
         crnt: Trits object as input
         func: function to apply
         return: Trist object containing result
-    '''' 
+    '''
 
-    next = "" 
+    next = ""
     for i in range(0, 3):
         next = next + map_str[basic_f_t[func][map_t[crnt[i]]]]
 
-    return Trits(next)
- 
+    return Trits.Trits(next)
+
 
 def get_unary(desired):
     ''' get_unary: get expression for a unary function
         desired: string representation of desired function
         returns: list of functions to apply to get the desired function
-    ''''
+    '''
 
+    # do some input error checking
     if len(desired) != 3:
         print "truth table must be 3 chars wide"
         raise SystemExit
+
+    for i in range(0, 3):
+        if not desired[i] in valid_chars:
+            print "%s not a valid character" % desired[i]
+            raise SystemExit
 
     goal = Trits.Trits(desired)
     crnt = Trits.Trits("i01")
     l_gates = []
 
+    for i in easy_f_t:
+        if goal.equals(easy_f_t[i]):
+            return True, [i]
+
     count = 0
-    end_cond = true
-    while end_cond:
-        end_cond, gates = recurse_unary(crnt, goal, l_gates, count)
+    end_cond = True
+    while end_cond and count < 4:
+        end_cond, l_gates = recurse_unary(crnt, goal, [], count)
         end_cond = not end_cond
         count = count + 1
 
-def recurse_unary(crnt, goal, l_gates, count):
+    return (not end_cond, l_gates)
 
-    if count == 0:
+def recurse_unary(crnt, goal, l_gates, depth):
+    ''' recurse_unary: attempt to locate a function sequence that makes the
+            goal function
+        crnt: current function result
+        goal: the desired function result
+        l_gates: list of gates to apply to get to crnt
+        count: the number of gate levels
+    '''
+
+    # check the basic gates for an answer
+    if depth == 0:
         return test_all(crnt, goal, l_gates)
 
     else:
+        # for each basic gate attempt to find the desired function
         for i in basic_f_t:
             cndt = eval_one(crnt, i)
             l_gates.append(i)
 
             if cndt.equals(goal):
-                return true, l_gates
+                return True, l_gates
 
             status, gates = recurse_unary(cndt, goal, l_gates, depth - 1)
- 
-            if status
-                return true, gates
+
+            if status:
+                return True, gates
 
             l_gates.pop()
 
-     return false, []
+    return (False, [])
 
 def test_all(crnt, goal, l_gates):
-    
+    ''' test_all: attempt to find a basic function that fulfills the desired
+            gate
+        crnt: the current evaluation of the function
+        goal: the desired function result
+        l_gates: list of gates currently applied to crnt
+    '''
+
     for i in basic_f_t:
         cndt = eval_one(crnt, i)
+
+        # if desired function is found then return true and the list of gates
+        # to apply
         if cndt.equals(goal):
             l_gates.append(i)
-            return true, l_gates
+            return True, l_gates
 
-    return false, []
+    return False, []
+
+if __name__ == "__main__":
+    print get_unary("11i")
 
 # use a.pop() to remove last item from list
 # check for a max depth of 3 functions to apply to get desired behavior
