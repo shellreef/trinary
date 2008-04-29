6ece6e05e7a885c0870305e8a867b1f1466aec00
Jeff Connelly <shellreef+git@gmail.com>
2008-04-29 15:12:36 -0700

    Move eval.py from digital_simulator/ to tools/.

---
+++ tools/Expr.py
@@ -0,0 +1,88 @@
+#!/usr/bin/python
+# vim: set fileencoding=utf8
+# Created:20080216
+# By Jeff Connelly
+#
+# Trinary expression evaluator
+
+from Symbols import *
+from Trits import Trits
+
+DEBUG = False
+
+unary_functions = {
+        # Everyone agrees on
+        u"/": Trits("10i"),
+
+        # Mouftah-based 
+        u"∇": Trits("1ii"), u"└": Trits("1ii"),
+        u"∆": Trits("11i"), u"┘": Trits("11i"),
+        u"¬": Trits("001"),
+        u"⌐": Trits("i00"),
+
+        # Grubb-based
+        u"↘": Trits("ii0"),
+        u"↗": Trits("011"),
+        u"∩": Trits("01i"),
+        u"∪": Trits("10i"),
+
+        # Ternary-logic-minimization-literature-based
+        u"♨": Trits("01i")
+        }
+
+def evaluate_unary(function, inputs):
+    """Given a unary function number in a Trits object, f, pass
+    all the trits in the inputs object through the function."""
+
+    assert isinstance(function, Trits), \
+            "Need to pass a Trits object as function"
+    assert isinstance(inputs, Trits), \
+            "You must pass a Trits object in inputs"
+
+    outputs = []
+    for t in inputs:
+        outputs.append({
+                False: function[0],
+                None: function[1],
+                True: function[2]
+                }[t])
+
+    return Trits(outputs)
+
+class Expr(object):
+    def __init__(self, s):
+        assert len(s) > 0, "Empty string can't evaluate"
+
+        variable = s[-1]
+        assert variable.isalpha(), "Only accept expressions of form xxxxA, where x=unary operators, A=variable"
+        if DEBUG: print "Variable:", variable
+
+        # Evaluate gates from right-to-left
+        unary_gates = reversed(s[:-1])
+
+        # Start with identity function
+        total_unary = Trits("i01")
+
+        # Evaluate with unary function on the identity
+        for gate in unary_gates:
+            if DEBUG: print "Gate:", gate.encode('utf8')
+            total_unary = evaluate_unary(unary_functions[gate], total_unary)
+
+        # Now total_unary is the function# of all the unary gates
+        self.total_unary = total_unary
+
+    def evaluate(self, inputs):
+        return evaluate_unary(self.total_unary, inputs)
+
+if __name__ == "__main__":
+    s = u"⌐∇a"
+    print "Expression:", s.encode('utf8')
+    e = Expr(s)
+    print "Total unary function is:", e.total_unary
+    print
+
+    ins = Trits("iiiiiiiii00000001111111")
+    print "Passing in trit vector as 'a':"
+    print "a:\t", ins
+    print s.encode('utf8') + ":\t", e.evaluate(ins)
+
---
+++ tools/Symbols.py
@@ -0,0 +1,44 @@
+#!env python
+# vim: set fileencoding=utf8
+# Created:20080216
+# By Jeff Connelly
+#
+# Trinary-related symbols
+# See http://jeff.tk/wiki/Trinary/Symbols
+# Note: to print, .encode('utf8') first
+#
+# MORE IMPORTANT NOTE: This isn't needed most of the time.
+# Instead, just use the Unicode symbols directly. You can
+# do this if the second line of the file is:
+#   # vim: set fileencoding=utf8
+
+# TODO: Use literal values, actual characters, not escape
+
+# Trinary.cc-based unary
+ROTATE_UP = u"\u2229"
+ROTATE_DN = u"\u222a"
+SHIFT_UP = u"\u2197"
+SHIFT_DN = u"\2198"
+INVERT = u"/"
+
+# Mouftah-based unary
+FD = u"\u00ac"
+RD = u"\u2310"
+PTI = u"\u2518";        PTI2 = u"\u2518"
+NTI = u"\u2207";        NTI2 = u"\u2514"
+STI = u"/"
+
+# TODO: dyadic
+
+# Balanced base 9
+NONARY = {
+        -1: u"\u2460",
+        -2: u"\u2461",
+        -3: u"\u2462",
+        -4: u"\u2463",
+        0: u"0",
+        1: u"1",
+        2: u"2",
+        3: u"3",
+        4: u"4"}
+
